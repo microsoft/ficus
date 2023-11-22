cd19cbe83b6c7b8c83c7f17b610f1c78bbf0f0f3
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2023-11-22 10:54:40 -0800

    Initial render before loading projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,39 +13,47 @@ export default function Home() {
 	const router = useRouter()
 	const [createPullRequestStatus, createPullRequestOperations] = useCreatePullRequest()
 	const isBusy = createPullRequestStatus.progress === "busy"
-	const projects = getProjectManager().getAll()
+	const [projects, setProjects] = React.useState<readonly Project[] | null>(null)
+
+	React.useEffect(() => {
+		setProjects(getProjectManager().getAll())
+	}, [])
 
 	return (
 		<Content>
 			<h1>You change variables &amp; Ficus changes code.</h1>
-			{createPullRequestStatus.progress === "busy" ? (
+			{projects === null ? null : isBusy ? (
 				<Link href="/status">Working...</Link>
-			) : projects.length > 0 ? (
-				projects.map(project => (
-					<div key={project.manifestUrl}>
-						<h2>{project.name}</h2>
-						<div className={styles.horizontal}>
-							<AccentButton onClick={() => createFigmaPullRequest(project)} disabled={isBusy}>
-								Create a pull request
-							</AccentButton>
-						</div>
-					</div>
-				))
-			) : projects.length === 0 ? (
+			) : (
 				<>
-					<h2>It'll just take a few minutes to get started.</h2>
-					<p>You only have to do this once. Choose which page sounds more appropriate for you:</p>
-					<ul>
-						<li>
-							<a href="/help/onboarding/repo">Preparing your GitHub repo to work with Ficus</a> (recommended for engineers)
-						</li>
-						<li>
-							<a href="/help/onboarding/usage">Setting up Ficus and making your first pull request</a> (recommended for
-							designers)
-						</li>
-					</ul>
+					{projects.map(project => (
+						<div key={project.manifestUrl}>
+							<h2>{project.name}</h2>
+							<div className={styles.horizontal}>
+								<AccentButton onClick={() => createFigmaPullRequest(project)} disabled={isBusy}>
+									Create a pull request
+								</AccentButton>
+							</div>
+						</div>
+					))}
+					{projects.length === 0 ? (
+						<>
+							<h2>It'll just take a few minutes to get started.</h2>
+							<p>You only have to do this once. Choose which page sounds more appropriate for you:</p>
+							<ul>
+								<li>
+									<a href="/help/onboarding/repo">Preparing your GitHub repo to work with Ficus</a> (recommended for
+									engineers)
+								</li>
+								<li>
+									<a href="/help/onboarding/usage">Setting up Ficus and making your first pull request</a> (recommended
+									for designers)
+								</li>
+							</ul>
+						</>
+					) : null}
 				</>
-			) : null}
+			)}
 		</Content>
 	)
 
