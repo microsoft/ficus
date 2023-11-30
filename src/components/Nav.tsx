d304ade1f561532cf7ac7d29308372ea3e8ff29f
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2023-11-29 16:39:21 -0800

    New top nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,32 +1,38 @@
+"use client"
+
 import type React from "react"
 import styles from "./Nav.module.css"
 import Link from "next/link"
-import { Content, ContentStack } from "./ContentStack"
+import { Subtitle2 } from "@fluentui/react-components"
 
 export interface NavProps {}
 
 export function Nav(_props: NavProps) {
 	return (
 		<>
-			<nav className={styles.root}>
-				<ContentStack>
-					<Content>
-						<ul className={styles.horiz}>
-							<li>
-								<Link href="/" className={styles.logo}>
-									Ficus
-								</Link>
-							</li>
-							<li>
-								<Link href="/settings">Settings</Link>
-							</li>
-							<li>
-								<Link href="/help">FAQ</Link>
-							</li>
-						</ul>
-					</Content>
-				</ContentStack>
-			</nav>
+			<header className={styles.root}>
+				<nav>
+					<ul className={styles.horiz}>
+						<li className={styles.logo}>
+							<Link href="/">
+								<img src="/images/app/logo48.png" width={48} height={48} alt="" />
+								<Subtitle2>Ficus</Subtitle2>
+							</Link>
+						</li>
+						<li>
+							<Link href="/settings">
+								<Subtitle2 style={{ fontWeight: "unset" }}>Settings</Subtitle2>
+							</Link>
+						</li>
+						<div className={styles.spacer}></div>
+						<li className={styles.right}>
+							<Link href="/help">
+								<Subtitle2 style={{ fontWeight: "unset" }}>FAQ</Subtitle2>
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</header>
 			<div className={styles.under} />
 		</>
 	)
