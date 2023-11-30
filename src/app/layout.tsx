21d828946e4980a58203464b81f85aa2db885fa9
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2023-11-29 17:05:06 -0800

    Page footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./styles.css"
 import { FluentProvider, ficusTheme } from "@/components/fluentui"
 import Nav from "@/components/Nav"
+import Footer from "@/components/Footer"
 import { ContentStack } from "@/components/ContentStack"
 
 export const metadata = {
@@ -16,11 +17,20 @@ export default function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<html lang="en">
 			<body>
-				<FluentProvider theme={ficusTheme} style={{ backgroundColor: "unset" }}>
+				<FluentProvider
+					theme={ficusTheme}
+					style={{
+						backgroundColor: "unset",
+						flex: "1",
+						display: "flex",
+						flexDirection: "column",
+					}}
+				>
 					<Nav />
 					<main>
 						<ContentStack>{children}</ContentStack>
 					</main>
+					<Footer />
 				</FluentProvider>
 			</body>
 		</html>
---
+++ src/components/Footer.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import type React from "react"
+import styles from "./Footer.module.css"
+import Link from "next/link"
+import { Subtitle2 } from "@fluentui/react-components"
+
+export interface FooterProps {}
+
+export function Footer(_props: FooterProps) {
+	return (
+		<>
+			<footer className={styles.root}>
+				<ul className={styles.horiz}>
+					<li>
+						<Link href="/">Ficus</Link> by{" "}
+						<a href="https://microsoft.design" target="_blank">
+							Microsoft Design
+						</a>
+					</li>
+					<div className={styles.spacer}></div>
+					<li>
+						<a href="https://go.microsoft.com/fwlink/?LinkID=206977" target="_blank">
+							Terms of use
+						</a>
+					</li>
+					<li>
+						<a href="https://go.microsoft.com/fwlink/?LinkId=521839" target="_blank">
+							Privacy
+						</a>
+					</li>
+					<li>
+						<a href="https://github.com/microsoft/ficus" target="_blank">
+							GitHub
+						</a>
+					</li>
+				</ul>
+			</footer>
+		</>
+	)
+}
+export default Footer
